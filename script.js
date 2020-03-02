6b37918d43cec2efe02d086358ebbe327414aa40
carolin.labinski <carolin.labinski@yahoo.de>
2020-03-02 21:51:12 +0100

    add3

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-// var footerEl = document.getElementsByTagName("footer")[0];
-
-// var onFooterClick = function() {
-//     console.log("clique");
-//    };
-
-//  footerEl.addEventListener("click", onFooterClick);
-
 // exo 1
 var footerEl = document.getElementsByTagName("footer")[0];
 
@@ -33,21 +25,12 @@ HamburgerBtn.addEventListener("click", onHamburgerBtnClick);
 
 // exo 3
 var EditBtn = document.getElementsByClassName("btn btn-sm btn-outline-secondary")[0];
-var TexteCard = document.getElementsByClassName("card-text")[0];
+var TexteCard1 = document.getElementsByClassName("card-text")[0];
 var clickOnEdit = function () {
-    TexteCard.style.color = "red"
+    TexteCard1.style.color = "red"
 };
 
 EditBtn.addEventListener("click", clickOnEdit);
-
-// exo 4
-var EditBtn2 = document.getElementsByClassName("btn btn-sm btn-outline-secondary")[1];
-var TexteCard = document.getElementsByClassName("card-text")[1];
-var clickOnEdit2 = function () {
-    TexteCard.style.color = "green"
-};
-
-EditBtn2.addEventListener("click", clickOnEdit2);
 
 var EditBtn2 = document.getElementsByClassName("btn btn-sm btn-outline-secondary")[1];
 var TexteCard = document.getElementsByClassName("card-text")[1];
@@ -59,27 +42,36 @@ var clickOnEdit2 = function () {
 EditBtn2.addEventListener("click", clickOnEdit2);
 
 // exo 5
-// var Navbar2 = document.getElementsByClassName("navbar navbar-dark bg-dark box-shadow")[0];
-// var link = document.getElementsByTagName("link")[0];
-// var disableLink = function () {
-//   link.href = ' ';
-// };
+var Navbar2 = document.getElementsByClassName("navbar navbar-dark bg-dark box-shadow")[0]
+ var link = document.getElementsByTagName("link")[0]
+ var disableLink = function (){
+    if (link.disabled == true) {
+    link.disabled = false
+    } else { 
+    link.disabled = true
+}
+ }
 
-// Navbar2.addEventListener("dblclick", disableLink);
+ Navbar2.addEventListener("dblclick", disableLink);
 
-var Navbar2 = document.getElementsByClassName("navbar navbar-dark bg-dark box-shadow")[0];
-var link = document.getElementsByTagName("link")[0];
-var disableLink = function () {
-    if (link.href === "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"){
-        link.href = ' ' ;
-        } else {link.href === "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"}    
-};
+ //exo 6
+const BtnView = document.getElementsByClassName("btn btn-sm btn-success");
+ const card = document.getElementsByClassName("card");
+ const cardText = document.getElementsByClassName('card-text');
 
-Navbar2.addEventListener("dblclick", disableLink);
+for(let i = 0; i < BtnView.length; i++){
+    BtnView[i].addEventListener("mouseover", function(){
+      cardText[i].classList.toggle("collapse");
+      if (card[i].style.width === "20%"){
+      card[i].style.width = "100%";}
+     else { 
+         card[i].style.width = "20%";
+        }
+ });
+    }
 
-// if (TexteCard.style.color === 'green'){
-//     TexteCard.style.color = '' ;
-//     } else {TexteCard.style.color = "green"}    
+
+
 
  
 
